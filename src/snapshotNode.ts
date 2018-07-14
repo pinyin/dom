9f4b939c5505f5eb06f6e9c7ff75762b8a7336f6
pinyin <bob432091@gmail.com>
2018-07-14 09:10:18 +0800

    batch style read & write

--- src/snapshotNode.ts
+++ src/snapshotNode.ts
@@ -4,32 +4,21 @@ import {travel} from './travel'
 
 export function snapshotNode<T extends StyledElement>(node: T): T {
     const clonedNode = node.cloneNode(true) as StyledElement
-    const styleMap = new Map<StyledElement, { [key: string]: string }>()
-    const priorityMap = new Map<StyledElement, { [key: string]: string }>()
+    const styles = new Map<StyledElement, string>()
     for (const descendant of travel(clonedNode)) { // FIXME what's the computed styles of an element that's not in document?
         if (isStyledElement(descendant)) {
-            const computedStyle: CSSStyleDeclaration = getComputedStyle(descendant)
-            const style = {} as any
-            const priority = {} as any
-            Array.from(computedStyle).forEach(key => {
-                style[key] = computedStyle.getPropertyValue(key)
-                priority[key] = computedStyle.getPropertyPriority(key)
-            })
-            styleMap.set(descendant, style)
-            priorityMap.set(descendant, priority)
+            const computedStyle: string = getComputedStyle(descendant).cssText
+            styles.set(descendant, computedStyle)
         }
     }
     // batch read & write to prevent reflow
     for (const descendant of travel(clonedNode)) {
         if (isStyledElement(descendant)) {
-            const style = styleMap.get(descendant)
-            const priority = priorityMap.get(descendant)
-            if (notExisting(style) || notExisting(priority)) {
+            const style = styles.get(descendant)
+            if (notExisting(style)) {
                 throw new Error(`Style not present for ${descendant}`)
             }
-            Object.keys(style).forEach(key => {
-                descendant.style.setProperty(key, style[key], priority[key])
-            })
+            descendant.style.cssText = style
             descendant.setAttribute('class', '')
         }
     }
