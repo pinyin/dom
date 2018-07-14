965b9410f27c49ed3d1a4d46bb43f4c4400edcd1
pinyin <bob432091@gmail.com>
2018-07-14 08:40:41 +0800

    update snapshotNode to prevent reflow

--- src/snapshotNode.ts
+++ src/snapshotNode.ts
@@ -1,12 +1,36 @@
-import {consolidateComputedStyle} from './consolidateComputedStyle'
+import {notExisting} from '@pinyin/maybe'
 import {isStyledElement, StyledElement} from './StyledElement'
 import {travel} from './travel'
 
 export function snapshotNode<T extends StyledElement>(node: T): T {
     const clonedNode = node.cloneNode(true) as StyledElement
+    const styleMap = new Map<StyledElement, { [key: string]: string }>()
+    const priorityMap = new Map<StyledElement, { [key: string]: string }>()
+    for (const descendant of travel(clonedNode)) { // FIXME what's the computed styles of an element that's not in document?
+        if (isStyledElement(descendant)) {
+            const computedStyle: CSSStyleDeclaration = getComputedStyle(descendant)
+            const style = {} as any
+            const priority = {} as any
+            Array.from(computedStyle).forEach(key => {
+                style[key] = computedStyle.getPropertyValue(key)
+                priority[key] = computedStyle.getPropertyPriority(key)
+            })
+            styleMap.set(descendant, style)
+            priorityMap.set(descendant, priority)
+        }
+    }
+    // batch read & write to prevent reflow
     for (const descendant of travel(clonedNode)) {
         if (isStyledElement(descendant)) {
-            consolidateComputedStyle(descendant, descendant)
+            const style = styleMap.get(descendant)
+            const priority = priorityMap.get(descendant)
+            if (notExisting(style) || notExisting(priority)) {
+                throw new Error(`Style not present for ${descendant}`)
+            }
+            Object.keys(style).forEach(key => {
+                descendant.style.setProperty(key, style[key], priority[key])
+            })
+            descendant.setAttribute('class', '')
         }
     }
     return clonedNode as T
--- src/travel.test.ts
+++ src/travel.test.ts
@@ -1,19 +1,19 @@
 import {travel} from './travel'
 
 
-describe(`${travel.name}`, () => {
-    const c1 = document.createElement('div')
-    const c2 = document.createElement('p')
-    const c3 = document.createElement('div')
-    const c4 = document.createElement('svg')
-    const c5 = document.createElement('path')
-    c1.appendChild(c2)
-    c1.appendChild(c3)
-    c3.style.width = '50px'
-    c3.appendChild(c4)
-    c4.appendChild(c5)
-    document.body.appendChild(c1)
+const c1 = document.createElement('div')
+const c2 = document.createElement('p')
+const c3 = document.createElement('div')
+const c4 = document.createElement('svg')
+const c5 = document.createElement('path')
+c1.appendChild(c2)
+c1.appendChild(c3)
+c3.style.width = '50px'
+c3.appendChild(c4)
+c4.appendChild(c5)
+document.body.appendChild(c1)
 
+describe(`${travel.name}`, () => {
     it(`should iterate over all descendant elements`, () => {
         expect(Array.from(travel(document.body)))
             .toEqual([document.body, c1, c2, c3, c4, c5])
